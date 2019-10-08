100695982296dc4f90d5490ba361d757798e43d8
Pablo Velasquez <maxpow2718r@pop-os.localdomain>
2019-10-08 12:01:43 -0300

    added moves

--- hanoi.py
+++ hanoi.py
@@ -1,21 +1,81 @@
-import curses
-import time
-from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN
 from torres import *
 
-n = 5
+n = int(input("cuantos discos: "))
+#n = 5
 T_A,T_B,T_C = torres().clavijas(n)
 
 pilares = [T_A, T_B, T_C]
+for x,y,z in zip(*pilares):
+    print x,y,z
 
-mover = 1
-
-if mover == 1:
+void = [(n * " ") + ("0") + (n * " ")]
+juego = True
+k = 2
+j = 0
+i = 0
+while juego:
+    direccion = str(raw_input("En que direccion desea moverse: "))
+    lista_reemplazo = []
     lista_paso = []
-    lista_paso.append(T_B[n-1])
-    T_B[n-1] = T_A[0]
-    T_A[0] = lista_paso[0]
-    for x,y,z in zip(*pilares):
-        print x,y,z
-    mover = 0
-    lista_paso = []
+    torre_actual = []
+    torre_destino = []
+    if direccion == "a":
+        torre_actual = pilares[i]
+        torre_destino = pilares[i - 1]
+        lista_paso.append(torre_destino[n + 1])
+        torre_destino[n + 1] = torre_actual[n + 1]
+        torre_actual[n + 1] = lista_paso[0]
+        for x,y,z in zip(*pilares):
+            print x,y,z
+        i = i - 1
+    if direccion == "d":
+        torre_actual = pilares[i]
+        torre_destino = pilares[i + 1]
+        lista_paso.append(torre_destino[n + 1])
+        torre_destino[n + 1] = torre_actual[n + 1]
+        torre_actual[n + 1] = lista_paso[0]
+        for x,y,z in zip(*pilares):
+            print x,y,z
+        i = i + 1
+    if direccion == "q":
+        juego = False
+    if direccion == "s":
+        torre_actual = pilares[i]
+        while j < n:
+            if torre_actual[j] != void[0]:
+                lista_paso.append(torre_actual[j])
+                direccion = str(raw_input("En que direccion desea moverse: "))
+                if direccion == "a":
+                    torre_destino = pilares[i - 1]
+                    lista_paso.append(torre_destino[n + 1])
+                    torre_destino[n + 1] = torre_actual[n + 1]
+                    torre_actual[n + 1] = lista_paso[1]
+                    while k < n + 2:
+                        torre_inversa = torre_destino.reverse()
+                        if torre_inversa[k] == void[0]:
+                            lista_reemplazo = torre_inversa[k]
+                            torre_destino[k] = lista_paso[0]
+                            k = n + 3
+                        else:
+                            k = k + 1
+                    torre_actual[j] = lista_reemplazo[0]
+                    for x,y,z in zip(*pilares):
+                        print x,y,z
+                    i = i - 1
+                if direccion == "d":
+                    torre_destino = pilares[i + 1]
+                    lista_paso.append(torre_destino[n + 1])
+                    torre_destino[n + 1] = torre_actual[n + 1]
+                    torre_actual[n + 1] = lista_paso[1]
+                    while k < n + 2:
+                        torre_inversa = torre_destino[::-1]
+                        if torre_inversa[k] == void[0]:
+                            lista_reemplazo = torre_inversa[k]
+                            torre_destino[k] = lista_paso[0]
+                            k = n + 3
+                        else:
+                            k = k + 1
+                    torre_actual[j] = lista_reemplazo[0]
+                    for x,y,z in zip(*pilares):
+                        print x,y,z
+                    i = i + 1
--- torres.py
+++ torres.py
@@ -1,7 +1,3 @@
-import curses
-import time
-from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN
-
 class torres(object):
 
     def __init__(self):
@@ -11,8 +7,8 @@ class torres(object):
 
     def clavijas(self,n):
         i = 0
-        while i <= n:
-            if i<n:
+        while i <= n + 1:
+            if i < n:
                 disk = 2*i + 1
                 spaces = n - (i)
                 self.T_A.append((spaces * " ") + (disk * "#") + (spaces * " "))
@@ -23,7 +19,11 @@ class torres(object):
                 self.T_B.append((2*i + 1) * "_")
                 self.T_C.append((2*i + 1) * "_")
 
+            elif i == n + 1:
+                self.T_A.append(((n) * " ") + ("*") + ((n) * " "))
+                self.T_B.append(((n) * " ") + (" ") + ((n) * " "))
+                self.T_C.append(((n) * " ") + (" ") + ((n) * " "))
+
             i += 1
 
         return self.T_A,self.T_B,self.T_C
-
